7dee523080a2512f9d6eceac107a70127af17d52
rryrrychu-uiuc <harryc3@illinois.edu>
2021-02-08 20:28:24 -0500

    Implemented the the deserialization of the pokemon class

--- src/main/java/com/example/Pokemon.java
+++ src/main/java/com/example/Pokemon.java
@@ -1,4 +1,52 @@
 package com.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class Pokemon {
+
+    private String name;
+    private String url;
+    private ArrayList<String> pokemonTypes;
+
+    public Pokemon(){ }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTypes() {
+        StringBuilder returnedPokemonTypes = new StringBuilder();
+        returnedPokemonTypes.append("Types: ");
+        for(String pokeType: pokemonTypes) {
+            returnedPokemonTypes.append(pokeType);
+            returnedPokemonTypes.append(" ");
+        }
+
+        return returnedPokemonTypes.toString();
+    }
+
+    public void extractPokemonStatsFromUrl() {
+
+        pokemonTypes = new ArrayList<>();
+        try {
+            JsonNode allPokemonStats = new ObjectMapper().readValue(new URL(url), JsonNode.class);
+            JsonNode pkmnTypeList = allPokemonStats.get("types");
+            for(JsonNode type: pkmnTypeList) {
+                JsonNode typeStats = type.get("type");
+                String typeName = typeStats.get("name").asText();
+                pokemonTypes.add(typeName);
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Invalid url");
+        }
+    }
 }
