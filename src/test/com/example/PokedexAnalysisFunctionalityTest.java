ae5da3aa5cc3c9c78dba091bc62a574c243809ab
rryrrychu-uiuc <harryc3@illinois.edu>
2021-02-09 13:58:53 -0500

    Separated filter and analysis test cases

---
+++ src/test/com/example/PokedexAnalysisFunctionalityTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PokedexAnalysisFunctionalityTest {
+
+    public static Pokedex original151Pokedex;
+
+    @BeforeClass
+    public static void setUp() {
+
+        original151Pokedex = new Pokedex("src/main/resources/original151pokemon.json");
+    }
+
+    @Test
+    public void testCorrectNumberOfPokemon() {
+        int expectedNumOfGrassTypes = 14;
+        int numOfPokemon = Pokedex.numOfPokemonWithSpecificType(original151Pokedex, "grass", false);
+        assertEquals(expectedNumOfGrassTypes, numOfPokemon);
+    }
+}
