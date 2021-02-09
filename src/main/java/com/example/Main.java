927add04788e8eb20dedf001a0c5ef1aa2a5da9e
rryrrychu-uiuc <harryc3@illinois.edu>
2021-02-08 20:29:24 -0500

    Implemented the container class for the Pokemon class

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,20 +1,8 @@
 package com.example;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-
-
-import java.io.File;
-import java.io.IOException;
-
-
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        File file = new File("src/main/resources/json");
-        //There is no need for an adventure class, the example below is just for convenience
-        //if you're using jackson. It's also a preview to your next assignment :)
-        //Adventure adventure = new ObjectMapper().readValue(file, Adventure.class);
-        //Gson gson = new Gson();
+    public static void main(String[] args){
+        Pokedex original151Pokedex = new Pokedex("src/main/resources/original151pokemon");
+        original151Pokedex.printAllPokemon();
     }
 }
--- src/main/java/com/example/Pokedex.java
+++ src/main/java/com/example/Pokedex.java
@@ -1,4 +1,37 @@
 package com.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 public class Pokedex {
+
+    private ArrayList<Pokemon> listOfPokemon;
+
+    public Pokedex(String path){
+
+        File file = new File(path);
+        try {
+            listOfPokemon = new ObjectMapper().readValue(file, new TypeReference<ArrayList<Pokemon>>(){});
+        } catch(IOException e) {
+            throw new IllegalArgumentException("Invalid path");
+        }
+
+        initializePokemonStats();
+    }
+
+    public void printAllPokemon() {
+        for(Pokemon toPrint: listOfPokemon) {
+            System.out.println(toPrint.getName() + " " + toPrint.getTypes());
+        }
+    }
+
+    private void initializePokemonStats() {
+        for(Pokemon toInitialize: listOfPokemon) {
+            toInitialize.extractPokemonStatsFromUrl();
+        }
+    }
 }
